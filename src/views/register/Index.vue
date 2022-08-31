3a0e7f307698c64b088deb8e3d1f365513c91dba
Dea9703 <dea9703@163.com>
2022-08-31 23:06:17 +0800

    完善登录注册切换样式

--- src/views/register/Index.vue
+++ src/views/register/Index.vue
@@ -22,20 +22,30 @@
       <a-form-item :wrapper-col="{ offset: 11 }">
         <a-button type="primary" html-type="submit" @click="submitForm(formState)">{{  register  }}</a-button>
       </a-form-item>
+      <div class="switch" @click="change">{{  isLogin ? '注册' : '登录'  }}</div>
     </a-form>
   </div>
 
 </template>
 
 <script setup>
-import { ref, reactive, getCurrentInstance } from 'vue';
+import { ref, reactive, getCurrentInstance, computed } from 'vue';
 import { message } from 'ant-design-vue';
 import { UserOutlined, LockOutlined } from '@ant-design/icons-vue';
 
 const { proxy } = getCurrentInstance()
 
-const register = ref('注册')
+let isLogin = ref(true)
+// const register = ref('注册')
 const formState = reactive({ account: '', password: '' })
+
+let register = computed(() => {
+  return isLogin.value ? '登录' : '注册'
+})
+
+const change = () => {
+  isLogin.value = !isLogin.value
+}
 
 const submitForm = async (formState) => {
   try {
@@ -68,6 +78,7 @@ const submitForm = async (formState) => {
   align-items: center;
 
   .ant-form {
+    position: relative;
     width: 500px;
     background-color: #ccc;
     border: 1px solid #ccc;
@@ -77,6 +88,21 @@ const submitForm = async (formState) => {
       margin-top: 14px;
       text-align: center;
       color: rgb(144, 151, 102);
+    }
+
+    .switch {
+      position: absolute;
+      right: -1px;
+      top: -1px;
+      display: inline-block;
+      padding: 0 20px;
+      height: 50px;
+      border-radius: 0px 10px 0 20px;
+      font-size: 18px;
+      color: rgb(227, 226, 226);
+      text-align: center;
+      line-height: 50px;
+      background-image: linear-gradient(43deg, #fff, #4b574b2f 20%, #99b84b8c 80%, #19b49fe5);
     }
   }
 }
