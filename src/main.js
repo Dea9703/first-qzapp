499bef8da905c909e0e322b1cdd0ec00a90df8ed
Dea9703 <dea9703@163.com>
2022-09-05 15:57:22 +0800

    feat: 新增pinia功能

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
-import { createApp } from 'vue'
-import Antd from 'ant-design-vue'
-import './assets/scss/reset.scss'
-import App from './App.vue'
-import router from './router'
-import axios from './utils/request.mjs'
+import { createApp } from "vue";
+import Antd from "ant-design-vue";
+import "./assets/scss/reset.scss";
+import App from "./App.vue";
+import router from "./router";
+import axios from "./utils/request.mjs";
+import { createPinia } from "pinia";
 
-import 'ant-design-vue/dist/antd.css'
+import "ant-design-vue/dist/antd.css";
 
-const app = createApp(App)
+const app = createApp(App);
 
 /** 挂载全局对象 - start */
-app.config.globalProperties.$axios = axios
+app.config.globalProperties.$axios = axios;
 /** 挂载全局对象 - end */
 
-app.use(router).use(Antd).mount('#app')
+app.use(createPinia()).use(router).use(Antd).mount("#app");
