19a1bac614913f87dd971ab95a796e10d5e5fb2b
Dea9703 <dea9703@163.com>
2022-09-06 17:56:06 +0800

    style: 调整代码格式

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,54 +1,58 @@
-'use strict'
-const userModel = require('../model/user')
+"use strict";
+const userModel = require("../model/user");
 
 class userController {
   /** 获取用户 */
   static async getUser(req, res) {
-    const uid = req.query.uid
+    const uid = req.query.uid;
 
-    let data
+    let data = {};
     if (!uid) {
       data = {
         code: 400,
-        message: 'uid is not exist'
-      }
+        message: "uid is not exist",
+      };
     } else {
-      data = await userModel.getUser(uid)
+      userInfo = await userModel.getUser(uid);
+      data = {
+        code: 200,
+        message: "query user success",
+        result: userInfo,
+      };
     }
-    res.send(data)
+    res.send(data);
   }
 
   /** 创建用户 */
   static async cteateUser(req, res) {
-    const { account, password } = req.body
-    let data
+    const { account, password } = req.body;
+    let data;
     if (!account || !password) {
       data = {
         code: 400,
-        message: 'none or all of the accounts and passwords exist'
-      }
-      res.send(data)
-      return
+        message: "none or all of the accounts and passwords exist",
+      };
+      res.send(data);
+      return;
     }
 
-    const user = await userModel.findUser(account)
+    const user = await userModel.findUser(account);
     if (user) {
       data = {
         code: 400,
-        message: 'the user already exists'
-      }
-      res.send(data)
-      return
+        message: "the user already exists",
+      };
+      res.send(data);
+      return;
     }
 
-
-    await userModel.createUser({ account, password })
+    await userModel.createUser({ account, password });
     data = {
       code: 200,
-      message: 'create user success'
-    }
+      message: "create user success",
+    };
 
-    res.send(data)
+    res.send(data);
   }
 
   /** 查找用户 */
@@ -58,4 +62,4 @@ class userController {
   // }
 }
 
-module.exports = userController
+module.exports = userController;
